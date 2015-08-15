b7bc8aa390972919a5582f8911184c5604e3a064
David Kühner <dav.kuhn@gmail.com>
2015-08-15 17:12:12 +0200

    Refactoring the whole implementation to lighter <a>.

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Facebook script 	
-(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.4";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
-
-// Xing script
-;(function (d, s) {var x = d.createElement(s),s = d.getElementsByTagName(s)[0];x.src = "https://www.xing-share.com/plugins/share.js";s.parentNode.insertBefore(x, s);})(document, "script");
-
-// Twitter script
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
