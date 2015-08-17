29d9393a47b75e20335f223eb5a1a8e97ba88daa
David Kühner <david.kuehner@akros.ch>
2015-08-17 14:27:29 +0200

    Add on appearance animations.

--- red/js/custom.js
+++ red/js/custom.js
@@ -1,1 +1,19 @@
 /* Add here all your JS customizations */
+
+/* On appear animations
+ * see : http://daneden.github.io/animate.css/
+ *       https://github.com/bas2k/jquery.appear
+ */
+jQuery(document).ready(function() {
+    $('.onAppearFadeIn').appear(function() {
+	  	$(this).addClass("animated fadeIn");
+	});
+	$('.onAppearFadeInRight').appear(function() {
+	  	$(this).addClass("animated fadeInRight");
+	});
+	/* Add more animation here : */
+	/* $('.onAppear<AnimateClassHere>').appear(function() {
+	 *  	$(this).addClass("animated <animateClassHere>");
+	 * 	});
+	 */
+});
